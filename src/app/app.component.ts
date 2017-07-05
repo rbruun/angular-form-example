521f3bf70d0af6f312c1f375a450eb78ec669899
Ron Bruun <ronald.bruun@libertymutual.com>
2017-07-05 11:23:11 -0400

    form validation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -8,10 +8,62 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   title = 'app';
+  sampleForm: NgForm;
+  @ViewChild('sampleForm') 
+  currentForm: NgForm;
+
+  validationMessages = {
+    'first_name': {
+      'required':      'First name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'last_name': {
+      'minlength':     'Name must be at least 2 characters long.'
+    }
+  };
 
 //app.component.ts
 onSubmit(data: NgForm){
   console.log(data.value)
 }
+
+ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.sampleForm) { return; }
+    //set the form to the current form for comparison
+    this.sampleForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.sampleForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+
+  onValueChanged(data?: any) {
+    let form = this.sampleForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an emtpy string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  };
+
 
 }
